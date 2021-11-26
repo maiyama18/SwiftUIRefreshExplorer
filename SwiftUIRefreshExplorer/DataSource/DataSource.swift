a5b117f2d167bc0e9c5d6b3ad4ac36a1665908ac
maiyama18 <myam0118@gmail.com>
2021-11-27 07:56:45 +0900

    implement SwiftUIPullToRefreshView

--- SwiftUIRefreshExplorer/DataSource/DataSource.swift
+++ SwiftUIRefreshExplorer/DataSource/DataSource.swift
@@ -5,11 +5,13 @@ class DataSource: ObservableObject {
     @Published var refreshing = false
     @Published var nums = Array(1...30)
     
-    func refresh() {
+    func refresh(completion: (() -> Void)? = nil) {
         refreshing = true
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             self.nums.shuffle()
             self.refreshing = false
+            
+            completion?()
         }
     }
 }
--- SwiftUIRefreshExplorer/Views/SwiftUIPullToRefresh/SwiftUIPullToRefreshViewController.swift
+++ SwiftUIRefreshExplorer/Views/SwiftUIPullToRefresh/SwiftUIPullToRefreshViewController.swift
@@ -1,8 +1,28 @@
-import UIKit
+import SwiftUI
+import SwiftUIPullToRefresh
 
 final class SwiftUIPullToRefreshViewController: UIViewController {
+    var dataSource: DataSource = .init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        
+        setupSwiftUIView(rootView: SwiftUIPullToRefreshView(dataSource: dataSource))
+    }
+}
+
+struct SwiftUIPullToRefreshView: View {
+    @ObservedObject var dataSource: DataSource
+    
+    var body: some View {
+        RefreshableScrollView(onRefresh: { done in
+            dataSource.refresh(completion: { done() })
+        }) {
+            VStack {
+                ForEach(dataSource.nums, id: \.self) { num in
+                    ItemView(num: num)
+                }
+            }
+        }
     }
 }
