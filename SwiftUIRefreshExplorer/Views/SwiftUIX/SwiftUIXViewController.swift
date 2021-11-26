bf3215caba1c4eaeea65c4d2a17913fb41a89115
maiyama18 <myam0118@gmail.com>
2021-11-27 07:39:56 +0900

    add pull to refresh to SwiftUIXView

--- SwiftUIRefreshExplorer/Views/SwiftUIX/SwiftUIXViewController.swift
+++ SwiftUIRefreshExplorer/Views/SwiftUIX/SwiftUIXViewController.swift
@@ -2,23 +2,27 @@ import SwiftUI
 import SwiftUIX
 
 final class SwiftUIXViewController: UIViewController {
+    var dataSource: DataSource = .init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupSwiftUIView(rootView: SwiftUIXView())
+        setupSwiftUIView(rootView: SwiftUIXView(dataSource: dataSource))
     }
 }
 
 struct SwiftUIXView: View {
-    @State var dataSource: DataSource = .init()
+    @ObservedObject var dataSource: DataSource
     
     var body: some View {
         CocoaScrollView {
-            LazyVStack {
+            VStack {
                 ForEach(dataSource.nums, id: \.self) { num in
                     ItemView(num: num)
                 }
             }
         }
+        .isRefreshing(dataSource.refreshing)
+        .onRefresh { dataSource.refresh() }
     }
 }
