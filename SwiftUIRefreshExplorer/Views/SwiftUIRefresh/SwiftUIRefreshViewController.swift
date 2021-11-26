0fdab4b6f4a80ebcd07d643a16499c1857dec1ba
maiyama18 <myam0118@gmail.com>
2021-11-27 07:50:48 +0900

    implement SwiftUIRefreshView

--- SwiftUIRefreshExplorer/Views/SwiftUIRefresh/SwiftUIRefreshViewController.swift
+++ SwiftUIRefreshExplorer/Views/SwiftUIRefresh/SwiftUIRefreshViewController.swift
@@ -1,8 +1,28 @@
-import UIKit
+import SwiftUI
+import SwiftUIRefresh
 
 final class SwiftUIRefreshViewController: UIViewController {
+    var dataSource: DataSource = .init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        
+        setupSwiftUIView(rootView: SwiftUIRefreshView(dataSource: dataSource))
+    }
+}
+
+struct SwiftUIRefreshView: View {
+    @ObservedObject var dataSource: DataSource
+    
+    var body: some View {
+        List {
+            ForEach(dataSource.nums, id: \.self) { num in
+                ItemView(num: num)
+            }
+        }
+        .listStyle(.plain)
+        .pullToRefresh(isShowing: $dataSource.refreshing) {
+            dataSource.refresh()
+        }
     }
 }
